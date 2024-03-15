08d6220b8cf2c6984cb47da0f310bb306b4bd215
VladSkopenko <skopirka2k17@gmail.com>
2024-03-15 15:44:28 +0300

    change urls

--- notes/noteapp/urls.py
+++ notes/noteapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'noteapp'
-
 urlpatterns = [
     path('', views.main, name='main'),
+    path('note/', views.note, name='note'),
     path('tag/', views.tag, name='tag'),
 ]
