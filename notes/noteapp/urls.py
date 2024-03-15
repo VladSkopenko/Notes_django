8fd1936dc5b40232476d8c0603ee8a61b246c46d
VladSkopenko <skopirka2k17@gmail.com>
2024-03-15 16:00:57 +0300

    added detail

--- notes/noteapp/urls.py
+++ notes/noteapp/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('note/', views.note, name='note'),
     path('tag/', views.tag, name='tag'),
+    path('detail/<int:note_id>', views.detail, name='detail'),
 ]
+
