55c7ca7ee607e87110ffd2551ea081996e860e43
VladSkopenko <skopirka2k17@gmail.com>
2024-03-16 14:49:03 +0300

    change settings

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'noteapp'
+    'noteapp',
+    'users'
 ]
 
 MIDDLEWARE = [
