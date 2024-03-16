578874581b0b2cc5d0aa41622bc56a79548db5fa
VladSkopenko <skopirka2k17@gmail.com>
2024-03-16 15:11:22 +0300

    addeda views loginuser

--- notes/users/views.py
+++ notes/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 
-from .forms import RegisterForm
-
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def register(request):
     if request.user.is_authenticated:
@@ -16,3 +17,18 @@ def register(request):
             return render(request, 'users/register.html', context={"form": form})
 
     return render(request, 'users/register.html', context={"form": RegisterForm()})
+
+def loginuser(request):
+    if request.user.is_authenticated:
+       return redirect(to='noteapp:main')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='noteapp:main')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
