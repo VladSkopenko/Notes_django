5dfce5fcd0944b45d3aa7488c95439d413173fdc
VladSkopenko <skopirka2k17@gmail.com>
2024-03-17 16:01:57 +0300

    change models and views

--- notes/noteapp/views.py
+++ notes/noteapp/views.py
@@ -1,11 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+
 from .forms import TagForm, NoteForm
 from .models import Tag, Note
-from django.contrib.auth.decorators import login_required
+
 
 def main(request):
     notes = Note.objects.filter(user=request.user).all() if request.user.is_authenticated else []
     return render(request, 'noteapp/index.html', {"notes": notes})
+
+
 
 @login_required
 def set_done(request, note_id):
