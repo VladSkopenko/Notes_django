fa11062eb26a529d42d92522704ee982235814c7
VladSkopenko <skopirka2k17@gmail.com>
2024-03-17 16:19:35 +0300

    finish base app

--- notes/users/forms.py
+++ notes/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
 from .models import Profile
 
 
@@ -22,12 +23,12 @@ class RegisterForm(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
-
     class Meta:
         model = User
         fields = ['username', 'password']
 
-...
+
+
 
 class ProfileForm(forms.ModelForm):
     avatar = forms.ImageField(widget=forms.FileInput())
